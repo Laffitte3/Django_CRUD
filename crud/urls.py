f2415ff05f635d41a2333514e3d7f0c26fb4faff
Alejandro <alejandromerlina324@gmail.com>
2024-06-08 11:25:44 -0700

    no me acuerdo que cambie

--- crud/urls.py
+++ crud/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from crud.views import BlogListView
 from crud.views import Consejos
 from crud.views import BlogDetailView
+from crud.views import BlogCreateView
+from crud.views import BlogUpdateView
+from crud.views import BlogDeleteView
 
 #statics
 from django.conf import settings
@@ -15,6 +18,11 @@ urlpatterns=[
     path("",BlogListView.as_view(),name="home"),
     path("consejo",Consejos.as_view(),name="consejos"),
     path("post/<int:pk>/",BlogDetailView.as_view(),name="detail"),
+    path("post/new/",BlogCreateView.as_view(),name="post_new"),
+    path("post/<int:pk>/edit",BlogUpdateView.as_view(),name="post_edit"),
+    path("post/<int:pk>/delete",BlogDeleteView.as_view(),name="post_delete"),
+    #path()
+
 
 ]
 
--- crud/views.py
+++ crud/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import ListView,DetailView,TemplateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from crud.models import Post
 from django.http import HttpResponse
+
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -14,6 +17,37 @@ class BlogDetailView(DetailView):
 
     model = Post
     template_name= "post_detail.html"
+
+class BlogCreateView(CreateView):
+
+    model= Post
+
+    template_name= "post_new.html"
+
+    fields=["title","body","author"]
+
+    success_url = reverse_lazy("home")
+
+
+class BlogUpdateView(UpdateView):
+
+    model=Post
+
+    template_name= "post_edit.html"
+
+    fields = ["title","body"]
+
+    success_url = reverse_lazy("home")
+
+
+class BlogDeleteView(DeleteView):
+
+    template_name= "post_delete.html"
+
+    model= Post
+
+    success_url = reverse_lazy("home")
+
 
 
 class Consejos(TemplateView):
